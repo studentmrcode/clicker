8c5c8e8cfea9d07b0bd3f2791a45153d89873cb5
David <davidk@mrcodeswildride.com>
2018-11-19 16:40:09 +0000

    init

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// Section 1: Make separate variables for the text box, button, and paragraph.
+// Section 1: Make separate variables for the image, paragraphs, and buttons.
 // You can name them anything you want, but for simplicity, name them the same as the IDs.
 
 
-// Section 2: Add an event listener to the button to show the story.
-// You can name the function anything you want, but pick something that indicates action, like showStory.
+// Section 2: Make separate variables to store the number of points, points per click, auto clicks, and upgrade costs.
+// Give the variables logical starting values (i.e. what the user starts with).
 
 
-// Section 3: Make a function to show the story.
+// Section 3: Add separate event listeners to the image and buttons.
+// You can name the functions anything you want, but pick something that indicates action.
+
+
+// Section 4: Make separate functions to add points, points per click, and auto clicks.
